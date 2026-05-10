02da1e5a5463e1bd71803c1c0ce59ca4cb852bf6
GoldenSpringFox <avivg2001@gmail.com>
2026-05-10 12:53:35 +0300

    added venv requirements

--- Project/scraper.py
+++ Project/scraper.py
@@ -1,5 +1,3 @@
-
-# Resources:
 # Scryfall API Wrapper: https://github.com/NandaScott/Scrython
 # EDHRec Wrapper: https://pypi.org/project/pyedhrec/
 # Archidekt Wrapper: https://github.com/linkian209/pyrchidekt
