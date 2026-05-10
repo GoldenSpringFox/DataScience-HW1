0766979672d13e965aaa47df9fb09b25a634db12
GoldenSpringFox <46161299+GoldenSpringFox@users.noreply.github.com>
2026-05-10 11:37:05 +0300

    project scraper start

---
+++ Project/scraper.py
@@ -0,0 +1,8 @@
+
+# Resources:
+# Scryfall API Wrapper: https://github.com/NandaScott/Scrython
+# EDHRec Wrapper: https://pypi.org/project/pyedhrec/
+# Archidekt Wrapper: https://github.com/linkian209/pyrchidekt
+
+if __name__ == "__main__":
+    print("Hello World")
